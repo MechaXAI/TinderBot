c8aff2aec4aa5fcd454ccb75b08aaf8fba0a6f2e
MechaXAI <adrianxalzamora@gmail.com>
2022-09-28 12:38:35 -0500

    Upload repo

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-# TinderBot
+# TinderBot automated
+
+* Tecnologies used in this PJ:
+  * Selenium
+* Create a login_details.py file
+* Introduce the following credentials
+
+```
+email = 'ur@gmail.com'
+password = 'urpwd'
+```
+
+* Constributions are welcome!
--- tinder_bot.py
+++ tinder_bot.py
@@ -1,14 +1,15 @@
 from asyncio.format_helpers import extract_stack
 from selenium import webdriver
-from time import sleep 
+from selenium.webdriver.common.by import By
+from time import sleep
 from login_details import email, password
 from selenium.webdriver.common.keys import Keys
 import random
 
+
 class TinderBot():
-    def __init__(self): # automatically run
-        self.driver = webdriver.Chrome()  #control de browser
-    
+    def __init__(self):  # automatically run
+        self.driver = webdriver.Chrome()  # control de browser
 
     # Test open tinder
     def open_tinder(self):
@@ -16,28 +17,32 @@ class TinderBot():
 
         sleep(2)
 
-        login = self.driver.find_element('xpath','/html/body/div[1]/div/div[1]/div/main/div[1]/div/div/div/div/header/div/div[2]/div[2]/a/div[2]/div[2]') # select the login element
+        login = self.driver.find_element(
+            'xpath', '/html/body/div[1]/div/div[1]/div/main/div[1]/div/div/div/div/header/div/div[2]/div[2]/a/div[2]/div[2]')  # select the login element
         login.click()
         sleep(1)
         self.facebook_login()
 
         sleep(6)
         try:
-           allow_location_button = self.driver.find_element('xpath','/html/body/div[2]/main/div/div/div/div[3]/button[1]')
-           allow_location_button.click()
+            allow_location_button = self.driver.find_element(
+                'xpath', '/html/body/div[2]/main/div/div/div/div[3]/button[1]')
+            allow_location_button.click()
         except:
-          print('no location popup')
+            print('no location popup')
 
         try:
-           allow_notification_button = self.driver.find_element('xpath','/html/body/div[2]/main/div/div/div/div[3]/button[2]')
-           allow_notification_button.click()
+            allow_notification_button = self.driver.find_element(
+                'xpath', '/html/body/div[2]/main/div/div/div/div[3]/button[2]')
+            allow_notification_button.click()
         except:
-          print('no notification popup')
-            
+            print('no notification popup')
+
         sleep(1)
 
         # cookies close wd
-        cookies_denied_btn = self.driver.find_element('xpath','/html/body/div[1]/div/div[2]/div/div/div[1]/div[2]/button')
+        cookies_denied_btn = self.driver.find_element(
+            'xpath', '/html/body/div[1]/div/div[2]/div/div/div[1]/div[2]/button')
         cookies_denied_btn.click()
 
         sleep(5)
@@ -51,18 +56,18 @@ class TinderBot():
         mayb_after_btn.click()
         self.driver.switch_to.window(base_window) """
 
-
-        mayb_after_popup = self.driver.find_element('xpath','/html/body/div[2]/main/div/div/div[3]/button[2]/span')
+        mayb_after_popup = self.driver.find_element(
+            'xpath', '/html/body/div[2]/main/div/div/div[3]/button[2]/span')
         mayb_after_popup.click()
 
         """ add_to_principal_popup = self.driver.find_element('xpath','/html/body/div[2]/main/div/div[2]/button[2]/div[2]/div[2]')
         add_to_principal_popup.click()  """
 
-        
-    # fb login 
+    # fb login
     def facebook_login(self):
         # find and clck fb login btn
-        login_with_fb = self.driver.find_element('xpath','/html/body/div[2]/main/div/div[1]/div/div/div[3]/span/div[2]/button')
+        login_with_fb = self.driver.find_element(
+            'xpath', '/html/body/div[2]/main/div/div[1]/div/div/div[3]/span/div[2]/button')
         login_with_fb.click()
 
         # save references to main and fb windows
@@ -75,13 +80,16 @@ class TinderBot():
 
         # find enter email, pwd, login
         # cookies fb
-            # cookies_accept_button = self.driver.find_element('xpath',)
-            # cookies_accept_button.click()
+        # cookies_accept_button = self.driver.find_element('xpath',)
+        # cookies_accept_button.click()
 
         # Login to FB
-        email_field = self.driver.find_element('xpath','/html/body/div/div[2]/div[1]/form/div/div[1]/div/input')
-        pwd_field = self.driver.find_element('xpath','/html/body/div/div[2]/div[1]/form/div/div[2]/div/input')
-        login_btn = self.driver.find_element('xpath','/html/body/div/div[2]/div[1]/form/div/div[3]/label[2]/input')
+        email_field = self.driver.find_element(
+            'xpath', '/html/body/div/div[2]/div[1]/form/div/div[1]/div/input')
+        pwd_field = self.driver.find_element(
+            'xpath', '/html/body/div/div[2]/div[1]/form/div/div[2]/div/input')
+        login_btn = self.driver.find_element(
+            'xpath', '/html/body/div/div[2]/div[1]/form/div/div[3]/label[2]/input')
 
         # enter email, pwd and login
         email_field.send_keys(email)
@@ -90,20 +98,19 @@ class TinderBot():
         self.driver.switch_to.window(base_window)
 
     def extra_popup(self):
-        add_to_principal_popup = self.driver.find_element('xpath','/html/body/div[2]/main/div/div[2]/button[2]/div[2]/span[2]')
-        add_to_principal_popup.click() 
+        self.driver.find_element(
+            By.CSS_SELECTOR, ".Mx\\(a\\) .l17p5q9z").click()
 
     def riht_swipe(self):
+        doc = self.driver.find_element('xpath', '//*[@id="Tinder"]/body')
+        doc.send_keys(Keys.ARROW_RIGHT)
         try:
-            doc = self.driver.find_element('xpath','//*[@id="Tinder"]/body')
-            doc.send_keys(Keys.ARROW_RIGHT)
-        except:
             self.extra_popup()
-            
-            
+        except:
+            print('Extra window popup not find')  # no sabia que colocar
 
     def dislike(self):
-        doc = self.driver.find_element('xpath','//*[@id="Tinder"]/body')
+        doc = self.driver.find_element('xpath', '//*[@id="Tinder"]/body')
         doc.send_keys(Keys.ARROW_LEFT)
 
     def auto_swipe(self):  # keep swiping
@@ -119,16 +126,15 @@ class TinderBot():
             else:
                 self.dislike()
 
-            random_sleep = random.random() *2
+            random_sleep = random.random() * 2
             sleep(random_sleep)  # time.sleep
-             
-            
 
     def close_match(self):
-        match_popup = self.driver.find_element('xpath','//*[@id="modal-manager-canvas"]/div/div/div[1]/div/div[3]/a')
+        match_popup = self.driver.find_element(
+            'xpath', '//*[@id="modal-manager-canvas"]/div/div/div[1]/div/div[3]/a')
         match_popup.click()
 
-    
+
 # mimicking human behaivour
 
 bot = TinderBot()
@@ -137,4 +143,4 @@ sleep(10)
 bot.auto_swipe()
 
 
-#based on https://github.com/tuomaskivioja/tinderBot/blob/master/tinder_bot.py
+# based on https://github.com/tuomaskivioja/tinderBot/blob/master/tinder_bot.py
